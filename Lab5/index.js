bc20e615dfc466065e4b46e81191064e44b8b424
Chris Dower <Chris Dower>
2016-03-09 15:27:16 -0500

    Added size limit to files and creation of multiple files

--- Lab5/index.js
+++ Lab5/index.js
@@ -1,5 +1,7 @@
 var Twitter = require('twitter');
 var express = require('express');
+//var http = require('http');
+var fs = require('fs');
 
 var client = new Twitter({
 	consumer_key: 'amgW24TB4ffSN5IPJFj5o1Ios',
@@ -8,12 +10,38 @@ var client = new Twitter({
 	access_token_secret: 'jrR7IKRBpIopRrl0onE0e5SHzQcFSiStwKko5pl6gZyl8'
 });
 
-var params = {screen_name: 'nodejs'};
-client.stream('statuses/filter', {track: 'javascript'}, function(stream) {
+var convention = 0;
+var searchTerm = 'president';
+var writeStream = fs.createWriteStream(searchTerm + '-' + convention + '-tweets.json', { flags : 'w'});
+writeStream.write("[");
+var count = 0;
+var max = 100;
+var writeBool = true;
+var activeStream;
+client.stream('statuses/filter', {track: searchTerm}, function(stream) {
 	stream.on('data', function(tweet){
-		console.log(tweet.text);
+		count++;
+		if(count >= max){
+			writeBool = false;
+			console.log(count);
+			writeStream.write(JSON.stringify(tweet) + ']json');
+			console.log("finished writing, closing stream");
+			writeStream.end();
+			console.log("Finished creating: " + searchTerm + convention + "-tweets.json");
+			convention++;
+			count = 0;
+			writeStream = fs.createWriteStream(searchTerm + convention + '-tweets.json', {flags : 'w'});
+			writeStream.write("[");
+			writeBool = true;
+		}
+		if(writeBool){
+			//console.log(tweet.text);
+			console.log(count);
+			writeStream.write(JSON.stringify(tweet)+',');
+		}
 	});
 	stream.on('error', function(error){
 		throw error;
+		stream.destroy();
 	});
 });
