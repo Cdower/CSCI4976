547ec0d27b9e8efeebbc4424d01cdd68f2cd0a37
Chris Dower <Chris Dower>
2016-03-08 21:42:54 -0500

    Lab 5

---
+++ Lab5/index.js
@@ -0,0 +1,18 @@
+vat Twitter = require('twitter');
+
+var client = new Twitter({
+	consumer_key: 'amgW24TB4ffSN5IPJFj5o1Ios',
+	consumer_secret: 'uptBeEV0UJP0eiE0blPnX8YZX7kQ7jdyflWEUtW1DLKqL9zxc3',
+	access_token_key: '371560459-b8wRiCsK3BeJ3fkciQP723R3j03kGoTbNOiOZQdE',
+	access_token_secret: 'jrR7IKRBpIopRrl0onE0e5SHzQcFSiStwKko5pl6gZyl8'
+});
+
+var params = {screen_name: 'nodejs'};
+client.stream('statuses/filter', {track: 'javascript'}, function(stream) {
+	stream.on('data', function(tweet){
+		console.log(tweet.text);
+	});
+	stream.on('error', function(error){
+		throw error;
+	});
+});
