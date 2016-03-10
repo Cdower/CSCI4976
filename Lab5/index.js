0aa45abe90c01ce59618c31578988031ade3c0e6
Chris Dower <devwowow@gmail.com>
2016-03-09 19:33:32 -0500

    JSON files now read in Lab1

--- Lab5/index.js
+++ Lab5/index.js
@@ -13,7 +13,7 @@ var client = new Twitter({
 var convention = 0;
 var searchTerm = 'president';
 var writeStream = fs.createWriteStream(searchTerm + '-' + convention + '-tweets.json', { flags : 'w'});
-writeStream.write("[\n");
+writeStream.write("[");
 var count = 0;
 var max = 100;
 var writeBool = true;
@@ -24,20 +24,20 @@ client.stream('statuses/filter', {track: searchTerm}, function(stream) {
 		if(count >= max){
 			writeBool = false;
 			console.log(count);
-			writeStream.write(JSON.stringify(tweet) + '\n]json');
+			writeStream.write(JSON.stringify(tweet) + ']');
 			console.log("finished writing, closing stream");
 			writeStream.end();
 			console.log("Finished creating: " + searchTerm + convention + "-tweets.json");
 			convention++;
 			count = 0;
 			writeStream = fs.createWriteStream(searchTerm + convention + '-tweets.json', {flags : 'w'});
-			writeStream.write("[\n");
+			writeStream.write("[");
 			writeBool = true;
 		}
 		if(writeBool){
 			//console.log(tweet.text);
 			console.log(count);
-			writeStream.write(JSON.stringify(tweet)+',\n');
+			writeStream.write(JSON.stringify(tweet) + ',');
 		}
 	});
 	stream.on('error', function(error){
