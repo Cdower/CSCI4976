a8c115dc0798f3341dff8101e5cbe9ce71a3a6f3
Cdower <cliffjumpers57@gmail.com>
2016-04-19 23:59:49 -0400

    added body-parser package to server for recieving json files

--- Lab8/index.js
+++ Lab8/index.js
@@ -1,6 +1,11 @@
 var express = require('express');//express
+var bodyParser = require('body-parser'); //parse or send json
 var app = expres();
 
 app.get('/', function (req, res) {
   res.sendFile( __dirname + '/index.html');
+});
+
+app.post('/queryDBPedia', function(req, res){
+  var query = req.body.query;
 });
