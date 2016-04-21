d3e6e3d2adf45bc5e7eec27be5f65ea5962013d9
Cdower <cliffjumpers57@gmail.com>
2016-04-21 13:22:42 -0400

    html now shows results, but only 2 columns. idk what to do with the query

--- Lab8/index.js
+++ Lab8/index.js
@@ -1,13 +1,24 @@
 var express = require('express');//express
 var bodyParser = require('body-parser'); //parse or send json
 var sparql = require('sparql');
-var app = expres();
-client = new sparql.Client 'http://dbpedia.org/sparql';
+var app = express();
+var client = new sparql.Client('http://dbpedia.org/sparql');
 
+app.use(bodyParser.json());
 app.get('/', function (req, res) {
   res.sendFile( __dirname + '/index.html');
 });
 
 app.post('/queryDBPedia', function(req, res){
-  var query = req.body.query;
+  var queryDB = req.body.query;
+  client.query('select distinct ?'+queryDB+' where {[] a ?'+queryDB+'} limit 10', function(err, response){
+    console.log('Response: ', response);
+    //console.log(row.s for row in response.results.bindings);
+    res.send(response);
+  });
+});
+
+
+app.listen(3000, function () {
+  console.log('Lab8 is listening on port 3000!');
 });
